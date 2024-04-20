e4424ae8c21f2fcc377cd94c70a9ff8d24c5c34e
Ashwin Solanki <solankiashwin789@gmail.com>
2024-04-20 11:22:00 +0530

    2 tabs implemented

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ $ cargo run TODO
 | Keys                      | Description                                     |
 | ------------------------- | ----------------------------------------------- |
 | <kbd>w</kbd>,<kbd>s</kbd> | Move up and down                                |
+| <kbd>></kbd>,<kbd><</kbd> | Switch to DONE and TODO tabs                    |
 | <kbd>q</kbd>              | Quit                                            |
 | <kbd>TAB</kbd>            | Switch between the TODO and DONE tabs           |
 | <kbd>Enter</kbd>          | Perform an action on the UI highlighted element |
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader, Write};
-use std::{env, process};
+use std::ops::{Add, Mul};
+use std::{cmp, env, process};
 // use std::os::unix::process;
 
 use ncurses::*;
@@ -10,52 +11,153 @@ const HIGHLIGHT_PAIR: i16 = 1;
 
 type Id = usize;
 
+#[derive(Default, Copy, Clone)]
+struct Demision {
+    x: i32,
+    y: i32
+}
+ 
+impl Add for Demision {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y
+        }
+    }
+}
+
+impl Mul for Demision {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self{
+        Self {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y
+        }
+    }
+}
+
+impl Demision {
+    fn new(x: i32, y: i32) -> Self {
+        Self {
+            x,
+            y
+        }
+    }
+}
+
+enum LayoutKind  {
+    Vert,
+    Horz
+}
+
+struct Layout {
+    kind: LayoutKind,
+    pos: Demision,
+    size: Demision,
+}
+
+impl Layout {
+    // fn new(kind: LayoutKind, pos: Demision) -> Self {
+    //     Self { kind, pos, size: Demision::new(0,0) }
+    // }
+
+    fn get_pos(&self) -> Demision {
+        use LayoutKind::*;
+        match self.kind {
+            Horz => self.pos + self.size * Demision::new(1,0),
+            Vert => self.pos + self.size * Demision::new(0,1),
+        }
+    }
+
+    fn add_widget(&mut self, size: Demision) {
+        use LayoutKind::*;
+        match self.kind {
+            Horz => {
+                self.size.x += size.x;
+                self.size.y = cmp::max(self.size.y, size.y);
+            },
+            Vert => {
+                self.size.x = cmp::max(self.size.x, size.x);
+                self.size.y += size.y;
+            },
+        }
+    }
+}
+
 #[derive(Default)]
 struct Ui {
-    list_curr: Option<Id>,
-    row: usize,
-    col: usize,
+    // list_curr: Option<Id>,
+    layouts: Vec<Layout>
 }
 
 impl Ui {
 
-    fn begin(&mut self, row: usize, col: usize) {
-        self.row = row;
-        self.col = col;
+    fn begin(&mut self,kind: LayoutKind, pos: Demision) {
+        assert!(self.layouts.is_empty());
+        self.layouts.push(Layout{
+            kind,
+            pos,
+            size: Demision::new(0,0) 
+        })
     }
 
-    fn begin_list(&mut self, id: Id) {
-        assert!(self.list_curr.is_none(), "Nested list are not allowed.");
-        self.list_curr = Some(id);
+    fn begin_layout(&mut self, kind: LayoutKind){ 
+        let layout = self.layouts.last().expect("Can't create a layout outside of Ui::begin() and Ui::end()");
+        let pos = layout.get_pos();
+
+        self.layouts.push(Layout{
+            kind,
+            pos,
+            size: Demision::new(0,0)
+        })
     }
+
+    // fn begin_list(&mut self, id: Id) {
+    //     assert!(self.list_curr.is_none(), "Nested list are not allowed.");
+    //     self.list_curr = Some(id);
+    // }
 
     fn label(&mut self, text: &str, pair: i16){
-        mv(self.row as i32, self.col as i32);
+        let layout = self.layouts.last_mut().expect("Trying to render label outside of any layouts.");
+        let pos = layout.get_pos();
+        mv(pos.y, pos.x);
 
         attron(COLOR_PAIR(pair));
         addstr(text).unwrap();
         attroff(COLOR_PAIR(pair));
 
-        self.row += 1;
+        layout.add_widget(Demision::new(text.len() as i32, 1));
     }
 
-    fn list_element(&mut self, text: &str, id: Id) {
-        let id_curr = self.list_curr.expect("Not allowed to create list element outside of lists");
-        self.label(text, if id_curr == id {
-            HIGHLIGHT_PAIR   
-        } else {
-            REGULAR_PAIR
-        });
-    }
+    // fn list_element(&mut self, text: &str, id: Id) {
+    //     let id_curr = self.list_curr.expect("Not allowed to create list element outside of lists");
+    //     self.label(text, if id_curr == id {
+    //         HIGHLIGHT_PAIR   
+    //     } else {
+    //         REGULAR_PAIR
+    //     });
+    // }
 
-    fn end_list(&mut self) {
-        self.list_curr = None;
-    }
+    // fn end_list(&mut self) {
+    //     self.list_curr = None;
+    // }
 
+    fn end_layout(&mut self){
+        let layout = self.layouts.pop().expect("Unbalanced Ui::begin_layout() and Ui::end_layout() calls.");
+        self.layouts.last_mut()
+            .expect("Unbalanced Ui::begin_layout() and Ui::end_layout() calls.")
+            .add_widget(layout.size);
+    }
+    
     fn end(&mut self) {
+        self.layouts.pop().expect("Unbalanced Ui::begin() and Ui::end() calls.");
     }
 }
 
+#[derive(PartialEq, PartialOrd)]
 enum Status {
     Todo,
     Done
@@ -67,6 +169,14 @@ impl Status {
             Status::Done => Status::Todo,
             Status::Todo => Status::Done
         }
+    }
+
+    fn right(self) -> Self {
+        Status::Done
+    }
+
+    fn left(self) -> Self {
+        Status::Todo
     }
 }
 
@@ -158,6 +268,9 @@ fn main() {
     initscr();
     noecho(); // To not echo the keys typed
 
+    // Allow for extended keyboard (like F1)
+    keypad(stdscr(), true);
+
     curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);  // make cursor invisible on terminal
 
     start_color();
@@ -169,38 +282,58 @@ fn main() {
 
     let mut ui = Ui::default();
 
+    const UP: char = KEY_UP as u8 as char;
+    const DOWN: char = KEY_DOWN as u8 as char;
+    const LEFT: char = KEY_LEFT as u8 as char;
+    const RIGHT: char = KEY_RIGHT as u8 as char;
+
     while !quit {
         erase(); // to clean terminal before printing anything.
-
-        ui.begin(0, 0);
+        ui.begin(LayoutKind::Horz,Demision::new(0, 0));
         {
-            match tab {
-                Status::Todo => {
-                    ui.label("[TODO] DONE", REGULAR_PAIR);
-                    ui.label("------------", REGULAR_PAIR);
-                    ui.begin_list(todo_curr);
-                    for (index, todo) in todos.iter().enumerate() {
-                        ui.list_element(&format!("- [ ] {}",todo), index);
-                    }
-                    ui.end_list();
-                },
-                Status::Done => {
-                    ui.label(" TODO [DONE]", REGULAR_PAIR);
-                    ui.label("------------", REGULAR_PAIR);
-                    ui.begin_list(done_curr);
-                    for (index, done) in dones.iter().enumerate() {
-                        ui.list_element(&format!("- [x] {}",done),index);
-                    }
-                    ui.end_list();
+            ui.begin_layout(LayoutKind::Vert);
+            {
+                ui.label("TODO:", REGULAR_PAIR);
+                // ui.label("------------", REGULAR_PAIR);
+                // ui.begin_list(todo_curr);
+                for (index, todo) in todos.iter().enumerate() {
+                    // ui.list_element(&format!("- [ ] {}",todo), index);
+                    ui.label(&format!("- [ ] {todo}"), 
+                        if index == todo_curr && tab == Status::Todo {
+                            HIGHLIGHT_PAIR
+                        } else {
+                            REGULAR_PAIR
+                        }
+                    )
                 }
             }
+            ui.end_layout();
+
+            ui.begin_layout(LayoutKind::Vert);
+            {
+                ui.label("DONE:", REGULAR_PAIR);
+                // ui.label("------------", REGULAR_PAIR);
+                // ui.begin_list(done_curr);
+                for (index, done) in dones.iter().enumerate() {
+                    // ui.list_element(&format!("- [x] {}",done),index);
+                    ui.label(&format!("- [*] {done}"), 
+                        if index == done_curr && tab == Status::Done {
+                            HIGHLIGHT_PAIR
+                        } else {
+                            REGULAR_PAIR
+                        }
+                    )
+                }
+            }
+            ui.end_layout();
+            // ui.end_list();
         }
         ui.end();
 
         refresh();
 
         let key = getch();
-        // println!("{key}");
+        // mv(20,0);
         match key as u8 as char {
             'q' => quit = true,
             'e' => {
@@ -212,13 +345,13 @@ fn main() {
                     writeln!(file, "DONE: {}", done).unwrap();
                 }
             },
-            'w' => {
+            'w' | UP => {
                 match tab {
                     Status::Todo => list_up(&mut todo_curr),
                     Status::Done => list_up(&mut done_curr)
                 }
             },
-            's' => {
+            's' | DOWN => {
                 match tab {
                     Status::Todo => list_down(&todos, &mut todo_curr),
                     Status::Done => list_down(&dones, &mut done_curr)
@@ -230,9 +363,18 @@ fn main() {
                     Status::Done => list_transfer(&mut dones, &mut todos, &mut done_curr)
                 }
             },
+            'i' => {
+                todo!();
+            },
             '\t' => {
                 tab = tab.toggle();
             },
+            RIGHT => {
+                tab = tab.right();
+            },
+            LEFT => {
+                tab = tab.left();
+            }
             _ => {}
         }
     }
